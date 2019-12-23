17b7ac4158cadb33be7a2ff247624499b016caef
Linuxsmurfen <linuxsmurfen@gmail.com>
2019-12-23 22:31:16 +0100

    Update demo.go

--- deployment/demo.go
+++ deployment/demo.go
@@ -13,7 +13,7 @@ import (
 var response string
 
 func defaultHandler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "Demo " + os.Getenv("OCP_TEXT"))
+        fmt.Fprintln(w, "Demo " + os.Getenv("K8S_TEXT"))
 }
 
 func jsonHandler(w http.ResponseWriter, r *http.Request) {
@@ -28,14 +28,14 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-        response = "{\n  \"Version\": \"" + os.Getenv("OCP_VERSION") +
-                    "\",\n  \"Text\": \"" + os.Getenv("OCP_TEXT") +
-                    "\",\n  \"Nodename\": \"" + os.Getenv("OCP_NODE_NAME") +
-                    "\",\n  \"Podname\": \"" + os.Getenv("OCP_POD_NAME") +
-                    "\",\n  \"Namespace\": \"" + os.Getenv("OCP_POD_NAMESPACE") +
-                    "\",\n  \"PodIP\": \"" + os.Getenv("OCP_POD_IP") +
-                    "\",\n  \"HostIP\": \"" + os.Getenv("OCP_HOST_IP") +
-                    "\",\n  \"Serviceaccount\": \"" + os.Getenv("OCP_POD_SERVICE_ACCOUNT") +
+        response = "{\n  \"Version\": \"" + os.Getenv("K8S_VERSION") +
+                    "\",\n  \"Text\": \"" + os.Getenv("K8S_TEXT") +
+                    "\",\n  \"Nodename\": \"" + os.Getenv("K8S_NODE_NAME") +
+                    "\",\n  \"Podname\": \"" + os.Getenv("K8S_POD_NAME") +
+                    "\",\n  \"Namespace\": \"" + os.Getenv("K8S_POD_NAMESPACE") +
+                    "\",\n  \"PodIP\": \"" + os.Getenv("K8S_POD_IP") +
+                    "\",\n  \"HostIP\": \"" + os.Getenv("K8S_HOST_IP") +
+                    "\",\n  \"Serviceaccount\": \"" + os.Getenv("K8S_POD_SERVICE_ACCOUNT") +
                     "\"\n}\n"
 
 	http.HandleFunc("/", defaultHandler)
